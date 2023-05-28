a5274234565faf438c34e34ca56fbbe77f692119
unknown <ricardoalexander1904gmail.com>
2023-05-28 18:39:39 -0500

    Ingreso de las bibliotecas y de las materias del ejercicio

---
+++ Ejercicio semana 10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char *materias[] = {
+    "Ingenieria de Software",
+    "Administracion",
+    "Economia",
+    "Relaciones Internacionales",
+    "Matematicas",
+    "Contabilidad",
+    "Ingenieria Industrial"};
